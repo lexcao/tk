82bc38a71fae01d2d1c06b780259f65ab5f6d4d2
Lex Cao <lexcao@foxmail.com>
2022-11-04 01:36:48 +0800

    fix footer position

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,19 @@ import PolarisProvider from "../components/PolarisProvider";
 import React from "react";
 import Footer from "../components/Footer";
 import {AnalyticsWrapper} from "../components/AnalyticsWrapper";
+import {Stack} from '@shopify/polaris';
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <PolarisProvider>
-      <Component {...pageProps} />
-
-      <footer style={{
-        padding: "1rem",
-        bottom: 0,
-        width: "100%",
-        position: "absolute"
-      }}>
-        <Footer/>
-      </footer>
-
+      <Stack vertical>
+        <main style={{minHeight: "90vh"}}>
+          <Component {...pageProps} />
+        </main>
+        <footer style={{padding: "0.6rem"}}>
+          <Footer/>
+        </footer>
+      </Stack>
       <AnalyticsWrapper/>
     </PolarisProvider>
   )
