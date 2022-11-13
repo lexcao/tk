416cd72ff44b36f756384c455a633c8f32a44108
Lex Cao <lexcao@foxmail.com>
2022-11-13 19:40:46 +0800

    dep: remove `kbar`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,16 @@ import type {AppProps} from 'next/app'
 import React from "react";
 import RootLayout from "../components/RootLayout";
 import Head from "next/head";
-import {KBarProvider} from "kbar";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <KBarProvider>
-      <RootLayout>
-        <Head>
-          <title>TK - Tag book marK</title>
-          <link rel="icon" href="/favicon.ico"/>
-          <meta name="description" content="Tag based bookmark manager"/>
-        </Head>
-        <Component {...pageProps} />
-      </RootLayout>
-    </KBarProvider>
+    <RootLayout>
+      <Head>
+        <title>TK - Tag book marK</title>
+        <link rel="icon" href="/favicon.ico"/>
+        <meta name="description" content="Tag based bookmark manager"/>
+      </Head>
+      <Component {...pageProps} />
+    </RootLayout>
   )
 }
